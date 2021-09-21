f6d1310ef8b92ba072c4ed4f826d0f0736c462e4
viettuanncool <60185465+viettuanncool@users.noreply.github.com>
2021-09-21 11:07:11 -0400

    added listings and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BodyComponent } from './body/body.component';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
 import { ListingMapComponent } from './listing/listing-map/listing-map.component';
 import { ListingDetailComponent } from './listing/listing-detail/listing-detail.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-=======
 import { AboutComponent } from './about/about.component';
-<<<<<<< HEAD
->>>>>>> 95373f36cf286a1aa88a3fa38233db9e400f40e8
-=======
 import { ContactComponent } from './contact/contact.component';
-import { FormsModule} from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
->>>>>>> eb675fcbb7a9363912ceaba80f1bff91a4ab3230
 
 @NgModule({
   declarations: [
@@ -31,31 +24,21 @@ import { HttpClientModule} from '@angular/common/http';
     HomeComponent,
     NavbarComponent,
     BodyComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     LoginComponent,
     ListingMapComponent,
-    ListingDetailComponent
-=======
-    AboutComponent
->>>>>>> 95373f36cf286a1aa88a3fa38233db9e400f40e8
-=======
+    ListingDetailComponent,
     AboutComponent,
-    ContactComponent
->>>>>>> eb675fcbb7a9363912ceaba80f1bff91a4ab3230
+    ContactComponent 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     FormsModule,
-<<<<<<< HEAD
     RouterModule,
     MatTabsModule,
     NoopAnimationsModule,
-=======
     HttpClientModule
->>>>>>> eb675fcbb7a9363912ceaba80f1bff91a4ab3230
   ],
   providers: [],
   bootstrap: [AppComponent]
