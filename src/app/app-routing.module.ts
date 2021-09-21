7616b47e9d1ec6a8c5880f7c14badc8e6f77e599
viettuanncool <60185465+viettuanncool@users.noreply.github.com>
2021-09-21 11:09:21 -0400

    fixed conflict

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { BodyComponent } from './body/body.component';
 import { HomeComponent } from './home/home.component';
 import { ListingDetailComponent } from './listing/listing-detail/listing-detail.component';
 import { ListingMapComponent } from './listing/listing-map/listing-map.component';
 import { LoginComponent } from './login/login.component';
-
-const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'listing-map', component: ListingMapComponent},
-  {path: 'listing-detail', component: ListingDetailComponent},
-=======
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
+
+
+
 
 const routes: Routes = [
   { path: '',   redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
-<<<<<<< HEAD
-  { path: 'about', component: AboutComponent}
->>>>>>> 95373f36cf286a1aa88a3fa38233db9e400f40e8
-=======
   { path: 'about', component: AboutComponent},
-  {path: 'contact', component:ContactComponent}
->>>>>>> eb675fcbb7a9363912ceaba80f1bff91a4ab3230
+  { path: 'about', component: AboutComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'listing-map', component: ListingMapComponent},
+  {path: 'listing-detail', component: ListingDetailComponent},
 ];
 
 @NgModule({
