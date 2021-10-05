c6451df1e33b088268e9dda42d8aee0828f85aab
zahidza <zahidza@sheridancollege.ca>
2021-10-05 09:34:07 -0400

    update to deploy project on heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BodyComponent } from './body/body.component';
 import { HomeComponent } from './home/home.component';
 import { ListingDetailComponent } from './listing/listing-detail/listing-detail.component';
 import { ListingMapComponent } from './listing/listing-map/listing-map.component';
@@ -14,7 +13,6 @@ import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
   { path: '',   redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
-  { path: 'about', component: AboutComponent},
   { path: 'about', component: AboutComponent},
   {path: 'contact', component:ContactComponent},
   {path: 'login', component: LoginComponent},
